bc46742a873082ce41590723933706f7533f24df
SusanHex <suehex4027@gmail.com>
2025-12-03 06:25:36 +0000

    Utilize the `config.GetConfigFromViper` function and print the messages after the delay for each message

--- src/main.go
+++ src/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "log/slog"
+import (
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/SusanHex/RE-Peater/config"
+	"github.com/spf13/viper"
+)
 
 func main() {
 	slog.Info("Welcome to RE-Peater!")
+	viper_instance := viper.NewWithOptions()
+	app_config, err := config.GetConfigFromViper(viper_instance)
+	if err != nil {
+		panic(err)
+	}
+	slog.Info(fmt.Sprintf("Found %d messages", len(app_config.Messages)))
+	slog.Info("Starting to write messages...")
+	for {
+		for _, log_message := range app_config.Messages {
+			time.Sleep(time.Duration(log_message.Delay) * time.Millisecond)
+			fmt.Println(log_message.Message)
+		}
+	}
 }
